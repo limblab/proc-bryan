fc00f92d11047f69d089cc82f92ecc7f6351fdb6
Bryan Yoder <blyoder@byoder.local>
2018-02-05 02:56:59 -0600

    1:

--- rat_analysis/SSEP_avg.m
+++ rat_analysis/SSEP_avg.m
@@ -1,7 +1,7 @@
 clear
 % File, PS/Battery, Amp, PW
 %SPIKEtargetfile = '171218_E2_SSEP1.plx'; %PS-1/.4
-SPIKEtargetfile = '171218_E2_SSEP2.plx'; %PS-2/.4
+SPIKEtargetfile = 'E3_180117_SSEP3.plx'; %PS-2/.4
 %SPIKEtargetfile = '171218_E2_SSEP3.plx'; %PS-.5/.4
 %SPIKEtargetfile = '171218_E2_SSEP4.plx'; %PS-.5/.4
 %SPIKEtargetfile = '171218_E2_SSEP5.plx'; %Battery-2/.4
@@ -23,7 +23,10 @@ fieldCh = [0:16];            % Specify the channels that contain field (LFP or E
 %
 filenames = dir(SPIKEtargetfile);
 filename = strtok(filenames(1).name,'.');
+filename2 = strtok(filenames(1).name);
+
 fielddata = import_plexon_analog([pwd '/'], filename, fieldCh); %Import LFPs
+[analogdata,spikedata] = import_plexon(filename2, [0:16], [1:16]);
 fielddata.timeframe = fielddata.timestamps(1) + single(0:1/fielddata.freq(1):(length(fielddata.data)-1)/fielddata.freq(1));
 %ts = mytimeseries;
 %ts.Data = fielddata.data'; 
@@ -36,24 +39,47 @@ dat = fielddata.data';
 
 % dat = right_dat;
 %stimind_t = find(abs(dat(:,17)) > .1); 
-stimind_t = find(dat(:,17) > .1); 
+%stimind_t = find(dat(:,17) > .1); 
 
+stimind_t = [];
 
-j=1;
-for i = 1:length(stimind_t)-1
-    if stimind_t(i+1) - stimind_t(i) > 1000
-        stimind(j) = stimind_t(i);
-        j = j+1;        
-    end    
+for i = 1:length(spikedata(1).ts)
+    numevents = 0;
+    for j = 2:16
+        numevents = numevents + ismember(spikedata(1).ts(i),spikedata(j).ts);
+    end
+    if numevents > 2
+       stimind_t(end+1) = spikedata(1).ts(i); 
+    end
 end
+
+% stimind_ttt=[];
+% for i = 1:338    
+%     stimind_ttt(end+1) = stimind_tt(i);
+%     if stimind_tt(i+1)-stimind_tt(i) > 1
+%         n = round(stimind_tt(i+1)-stimind_tt(i)) - 1;
+%         for j = 1:n
+%              stimind_ttt(end+1) = stimind_tt(i) + j*0.9985;
+%         end
+%     end
+% end
+
+% j=1;
+% for i = 1:length(stimind_t)-1
+%     if stimind_t(i+1) - stimind_t(i) > 1000
+%         stimind(j) = stimind_t(i);
+%         j = j+1;        
+%     end    
+% end
 
 % dat = left_dat;
 % stimind = find(abs(dat(:,17)) > .1); 
+stimind = round(stimind_t2 * 2000);
 
 dt = mean(diff(fielddata.timeframe));
 nstim = length(stimind)
-ind1 = 500;
-ind2 = 500;
+ind1 = 100;
+ind2 = 200;
 for jj = 1:16
     subplot(4,4,jj)
     for ii = 1:nstim
@@ -69,4 +95,4 @@ for jj = 1:16
 end
 subplot(4,4,1)
 title(strrep(SPIKEtargetfile,'_',' '))
-savefig([strrep(SPIKEtargetfile,'.plx','') '_avg.fig'])
+savefig([strrep(SPIKEtargetfile,'.plx','') '_avge.fig'])
